923924f05c88ddb3d611898a710963d6ac63ec57
Your Name <sandeep.pradhan99@gmail.com>
2024-05-19 22:20:52 +0530

    Completed i guess but i don't know what going on here lol

--- PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
+++ PlatformService/obj/Debug/net8.0/PlatformService.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfcf78531fa5b024f160e38b8d68b9f23b25a7a6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8357adfd621720aff03ea970b9f436e7896e2191")]
 [assembly: System.Reflection.AssemblyProductAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PlatformService")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
